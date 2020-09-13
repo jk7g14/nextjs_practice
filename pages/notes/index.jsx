ae13288d0d9e156bdb1e37102d72e704e45c241f
James <jk7g14@gmail.com>
2020-09-14 02:39:29 +0900

    day1 -03

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,6 +1,28 @@
+// pages/notes/index.jsx
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import Link from 'next/link'
 
-import React from 'react'
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({ id: i, title: `This is my note ${i}` }))
 
-const Page = () => <h1>Note Index Page</h1>
+  return (
+    <div sx={{ variant: 'containers.page' }}>
+      <h1>My Notes</h1>
 
-export default Page;
+      <div sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap' }}>
+        {notes.map(note => (
+          <div sx={{ width: '33%', p: 2 }}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{ textDecoration: 'none', cursor: 'pointer' }}>
+                <div sx={{ variant: 'containers.card', }}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
