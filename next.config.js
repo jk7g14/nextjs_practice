93f7328faca0ba29dd9bd027e8c9a1229f9a849b
james <jk7g14@gmail.com>
2020-09-21 00:48:28 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-//next config object
-const config = {}
-module.exports = withMyPlugin(config)
+// const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+// //next config object
+// const config = {}
+// module.exports = withMyPlugin(config)
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+
+dotenvLoad();
+
+const withEnv = nextEnv()
+module.exports = withEnv()
